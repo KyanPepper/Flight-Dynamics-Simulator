1c56ab3ad885ca884770b398c1af301a159bbd28
KyanPepper <Kyankks1@gmail.com>
2025-09-25 23:56:25 -0700

    Cmake build scipt and Hello world

---
+++ src/main_qt.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QLabel hello("Hello from Qt 6!");
+    hello.resize(320, 60);
+    hello.show();
+    return app.exec();
+}
